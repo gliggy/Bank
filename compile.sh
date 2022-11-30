0d3acdc9e06bcacef913d1dd4febce5891e121db
gliggy <leocfitz@gmail.com>
2022-11-29 22:00:11 -0500

    install javac

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 javac -classpath .:target/dependency/* -d . $(find . -type f -name '*.java')
 java -classpath .:target/dependency/* Main
+# for debian: sudo apt install openjdk-17-jdk-headless
