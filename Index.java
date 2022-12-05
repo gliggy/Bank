ca6038aa101d77114dabaa392d193b1b642226fa
gliggy <leocfitz@gmail.com>
2022-12-05 09:45:52 -0500

    Update Index.java

--- Index.java
+++ Index.java
@@ -6,9 +6,6 @@ public class Index {
     accounts.put("gliggy", new Account("gliggy","duckduck",500.0));
     accounts.put("leo", new Account("leo","password123",1500.0));
     accounts.put("paul", new Account("paul","secret",1050.0));
-    accounts.put("maman", new Account("maman","chien",55.0));
-    accounts.put("maman", new Account("maman","chien",455.0));
-    accounts.put("maman", new Account("maman","chien",0.0));
   }
   public void signup(String username, String password, double balance) {
     Account account = new Account(username,password,balance);
